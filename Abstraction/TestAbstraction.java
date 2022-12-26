c53fd122f56992f977cd6ce859dbccf0a2700890
gv18 <60931511+gv18@users.noreply.github.com>
2022-12-26 16:46:01 +0530

    Add files via upload

---
+++ Abstraction/TestAbstraction.java
@@ -0,0 +1,15 @@
+abstract class Shape{
+    abstract void draw();
+}
+class Rectangle extends Shape{
+    void draw(){System.out.println("drawing rectangle");}
+}
+class Circle1 extends Shape{
+    void draw(){System.out.println("drawing circle");}
+}
+public class TestAbstraction{
+    public static void main(String args[]){
+        Shape s=new Circle1();
+        s.draw();
+    }
+}
